535a9bd113edc22e352450d398aaa28f073956ed
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2016-09-22 16:13:20 -0500

    update setup.py for recent pip

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import pip
 from pip import req
 import setuptools
 
@@ -11,7 +12,9 @@ setuptools.setup(
     author_email="chris.a.st.pierre@gmail.com",
     packages=["crashes",
               "crashes.cmd"],
-    install_requires=[str(r.req)
-                      for r in req.parse_requirements("requirements.txt")],
+    install_requires=[
+        str(r.req)
+        for r in req.parse_requirements("requirements.txt",
+                                        session=pip.download.PipSession())],
     entry_points={
         "console_scripts": "crashes = crashes.cli:main"})
