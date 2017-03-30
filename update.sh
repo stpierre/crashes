e1813178a128275e932732f6819492b4346d94d8
Chris St. Pierre <cstpierr@cisco.com>
2017-03-30 13:06:37 -0500

    Reform update scripts

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+#
+# Update the downloaded and curated data
+
+if [[ ! -e crashes.sqlite ]]; then
+    crashes database restore
+fi
+
+crashes -v fetch --autostart
+
+echo "Parsing new PDFs"
+crashes -v parse
+
+if [ -t 1 ]; then
+    # stdout is a tty, so we can curate what we downloaded
+    crashes curate
+    crashes geocode
+    crashes hitnrun
+
+    # only generate results if we could do everything else, too
+    crashes xform
+    crashes results
+    crashes csvify
+fi
+
+crashes database dump
--- update_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-#
-# Update the results.
-
-git rebase master
-
-echo "Generating graph data and results"
-crashes xform
-crashes results
