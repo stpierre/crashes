88b21fe0c6e90c1f6a30a740fa934d366f059d68
Chris St. Pierre <cstpierr@cisco.com>
2017-08-07 11:48:06 -0500

    convert csvify to json db

--- crashes/commands/csvify.py
+++ crashes/commands/csvify.py
@@ -1,47 +1,91 @@
 """Collision database operations."""
 
 import csv
-import json
+import datetime
 import logging
+import operator
 import os
 
 import unicodecsv
 
 from crashes.commands import base
-from crashes import models
+from crashes import db
 
 LOG = logging.getLogger(__name__)
+
+
+def _collision_row_sort(row):
+    date = row[4]
+    if date is None:
+        date = datetime.date.fromtimestamp(0)
+    time = row[5]
+    if time is None:
+        time = datetime.time()
+    return (date, time)
+
+
+def _ticket_row_sort(row):
+    case_no = row[0]
+    report = db.collisions[case_no]
+    date = report.get("date") or datetime.date.fromtimestamp(0)
+    time = report.get("time") or datetime.time()
+    return (date, time)
 
 
 class CSVify(base.Command):
     """Collision database operations."""
 
     def dump_tickets(self):
-        output_path = os.path.join(self.options.csvdir,
-                                   "%s.csv" % models.Ticket.__tablename__)
-        LOG.info("Dumping data from %r table to %s",
-                 models.Ticket.__tablename__, output_path)
-        rows = 0
+        output_path = os.path.join(self.options.csvdir, "ticket.csv")
+        LOG.info("Dumping data from %s to %s",
+                 db.tickets.filename, output_path)
+        rows = []
+        for ticket in db.tickets:
+            report = db.collisions[ticket["case_no"]]
+            if report.get("road_location") in ("None", "not involved"):
+                continue
+            rows.append((ticket["case_no"], ticket["initials"], ticket["desc"]))
+        rows.sort(key=_ticket_row_sort)
+
         with open(output_path, "w") as outfile:
             writer = unicodecsv.writer(outfile, encoding="utf-8",
                                        dialect=csv.excel)
             writer.writerow(("Case #", "Initials", "Description"))
-            tickets = self.db.query(models.Ticket).join(
-                models.Collision).filter(
-                    models.Collision.road_location_name.isnot(None)).filter(
-                        models.Collision.road_location_name != "not involved").order_by(
-                            models.Collision.date, models.Collision.time).all()
-            for ticket in tickets:
-                writer.writerow((ticket.case_no, ticket.initials, ticket.desc))
-                rows += 1
-        LOG.info("Dumped %s rows to %s", rows, output_path)
+            writer.writerows(rows)
+        LOG.info("Dumped %s rows to %s", len(rows), output_path)
 
     def dump_collisions(self):
-        output_path = os.path.join(self.options.csvdir,
-                                   "%s.csv" % models.Collision.__tablename__)
-        LOG.info("Dumping data from %r table to %s",
-                 models.Collision.__tablename__, output_path)
-        rows = 0
+        output_path = os.path.join(self.options.csvdir, "collision.csv")
+        LOG.info("Dumping data from %s to %s",
+                 db.collisions.filename, output_path)
+        rows = []
+        for crash in db.collisions:
+            if crash.get("road_location") in (None, "not involved"):
+                continue
+            row = [crash["case_no"], crash.get("dob"), crash.get("gender"),
+                   crash.get("initials"), crash["date"], crash["time"]]
+            if crash.get("injury_region"):
+                row.append(db.injury_region[crash["injury_region"]]["desc"])
+            else:
+                row.append(None)
+            if crash.get("injury_severity"):
+                row.append(
+                    db.injury_severity[crash["injury_severity"]]["desc"])
+            else:
+                row.append(None)
+            row.append(crash.get("location"))
+            if crash.get("geojson"):
+                row.append(crash["geojson"].get("properties", {}).get("postal"))
+            else:
+                row.append(None)
+
+            row.extend(
+                [crash.get("latitude"), crash.get("longitude"),
+                 crash.get("hit_and_run"), crash.get("hit_and_run_status"),
+                 crash.get("road_location"), crash.get("report")])
+            rows.append(row)
+        rows.sort(key=_collision_row_sort)
+
         with open(output_path, "w") as outfile:
             writer = unicodecsv.writer(outfile, encoding="utf-8",
                                        dialect=csv.excel)
@@ -51,56 +95,29 @@ class CSVify(base.Command):
                              "Location", "ZIP", "Latitude", "Longitude",
                              "Hit and run?", "Hit and runner",
                              "Road location", "Report"))
-            crashes = self.db.query(models.Collision).filter(
-                models.Collision.road_location_name.isnot(None)).filter(
-                    models.Collision.road_location_name != "not involved").order_by(
-                        models.Collision.date, models.Collision.time).all()
-            for crash in crashes:
-                if crash.geojson:
-                    geojson = json.loads(crash.geojson)
-                else:
-                    geojson = {}
-                zipcode = geojson.get("properties", {}).get("postal")
-                row = [crash.case_no, crash.dob, crash.gender,
-                       crash.initials, crash.date, crash.time]
-                if crash.injury_region:
-                    row.append(crash.injury_region.desc)
-                else:
-                    row.append(None)
-                if crash.injury_severity:
-                    row.append(crash.injury_severity.desc)
-                else:
-                    row.append(None)
-                row.extend(
-                    [crash.location, zipcode, crash.latitude, crash.longitude,
-                     crash.hit_and_run, crash.hit_and_run_status_name,
-                     crash.road_location_name, crash.report])
-                writer.writerow(row)
-                rows += 1
-        LOG.info("Dumped %s rows to %s", rows, output_path)
+            writer.writerows(rows)
+        LOG.info("Dumped %s rows to %s", len(rows), output_path)
 
     def dump_traffic(self):
         for ttype in ("bike", "car"):
             output_path = os.path.join(self.options.csvdir,
-                                       "%s-%s.csv" % (
-                                           models.Traffic.__tablename__,
-                                           ttype))
-            LOG.info("Dumping data on %s traffic from %r table to %s",
-                     ttype, models.Traffic.__tablename__, output_path)
-            rows = 0
+                                       "traffic-%s.csv" % ttype)
+            LOG.info("Dumping data on %s traffic from %s to %s",
+                     ttype, db.traffic.filename, output_path)
+            rows = []
+            for record in db.traffic:
+                if record["type"] == ttype:
+                    rows.append((record["date"], record["start"],
+                                 record["end"],
+                                 record["count"], record["location"]))
+            rows.sort(key=operator.itemgetter(0, 1))
+
             with open(output_path, "w") as outfile:
                 writer = unicodecsv.writer(outfile, encoding="utf-8",
                                            dialect=csv.excel)
                 writer.writerow(("Date", "Start", "End", "Count", "Location"))
-                traffic = self.db.query(models.Traffic).filter(
-                    models.Traffic.type == ttype).order_by(
-                        models.Traffic.date,
-                        models.Traffic.start).all()
-                for record in traffic:
-                    writer.writerow((record.date, record.start, record.end,
-                                     record.count, record.location))
-                    rows += 1
-                LOG.info("Dumped %s rows to %s", rows, output_path)
+                writer.writerows(rows)
+                LOG.info("Dumped %s rows to %s", len(rows), output_path)
 
     def __call__(self):
         if not os.path.exists(self.options.csvdir):
